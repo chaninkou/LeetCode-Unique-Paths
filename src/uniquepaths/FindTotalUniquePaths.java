a50810de6504997ced1d3b6e224156faca760c77
chaninkou <michaelkoulove@yahoo.com>
2020-02-07 23:59:13 -0800

    Great reivew

--- src/uniquepaths/FindTotalUniquePaths.java
+++ src/uniquepaths/FindTotalUniquePaths.java
@@ -1,7 +1,31 @@
 package uniquepaths;
 
 public class FindTotalUniquePaths {
-    public int uniquePaths(int m, int n) {
+	// Dp way, but 2D array this time
+	// Basically check the maximum path per block to the end
+	public int uniquePath(int m, int n){
+		int[][] dp = new int[m][n];
+		
+		// Fill the top part with all 1's
+		for(int j = 0; j < n; j++){
+			dp[0][j] = 1;
+		}
+		
+		// Fill the left part with all 1's
+		for(int i = 0; i < m; i++){
+			dp[i][0] = 1;
+		}
+		
+		// No point of starting from 0, already fill those with 1
+		for(int i = 1; i < m; i++){
+			for(int j = 1; j < n; j++){
+				dp[i][j] = dp[i][] + dp[][];
+			}
+		}
+		
+	}
+	
+    public int uniquePaths2(int m, int n) {
         // Total number of steps to goal
         int N = n + m - 2;
         
